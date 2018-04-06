1639d6341fff15f66752151dec98027a1f385e7b
aviouslyl <lal@college.harvard.edu>
2018-04-05 20:23:44 -0400

    added csv

--- collector.py
+++ collector.py
@@ -7,14 +7,17 @@ headers = {
 	"X-API-Key" : "u9QEe02oYi9WonmL2dcufjr6dykCG8dt9maS89Qk"
 }
 
-for i in range(80,81):
-	# Make a get request 
-	response = requests.get("https://api.propublica.org/congress/v1/" + str (i) + "/senate/votes/party.json", headers = headers)
+#for i in range(80,116):
+i = 101
 
-	print(response.status_code) 
+# Make a get request 
+response = requests.get("https://api.propublica.org/congress/v1/" + str(i) + "/senate/votes/party.json", headers = headers)
 
-	if (response.status_code == 200):
-		data = response.json()
-		with open('congress' + str(i) + '.json', 'w') as outfile:
-		    json.dump(data, outfile)
+print(response.status_code) 
+
+if (response.status_code == 200):
+	data = response.json()
+	with open('congress' + str(i) + '.json', 'w') as outfile:
+	    print(data[results])
+	    json.dump(data, outfile)
 
